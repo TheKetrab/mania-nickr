1e2b8cbb6e696863e7b39f19b78bc750ef7e6229
Bartłomiej Grochowski <ketrab508@gmail.com>
2020-04-19 21:37:49 +0200

    https

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 // ----- pkg -----
 const fs           = require('fs');
-const http         = require('http');
+//const http         = require('http');
 const https        = require('https');
 const express      = require('express');
 const cookieParser = require('cookie-parser');
@@ -11,12 +11,11 @@ var certificate = fs.readFileSync('server/cert/server.crt', 'utf8');
 var credentials = {key: privateKey, cert: certificate};
 var app = express();
 
-var httpServer = http.createServer(app);
-//var httpsServer = https.createServer(credentials, app);
+//var httpServer = http.createServer(app);
+//httpServer.listen(process.env.PORT || 5000);
 
-//httpServer.listen(3000);
-httpServer.listen(process.env.PORT || 5000);
-//httpsServer.listen(3001);
+var httpsServer = https.createServer(credentials, app);
+httpsServer.listen(process.env.PORT || 5000);
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
