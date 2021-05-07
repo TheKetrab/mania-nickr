a3d3e1d543503acd0fb10d65c7a510aaf37ec11f
Bartłomiej Grochowski <ketrab508@gmail.com>
2021-05-08 00:38:03 +0200

    may 2021 update

--- server/blocks.js
+++ server/blocks.js
@@ -1,32 +1,168 @@
 
 class Block {
-    constructor(name,author,screen,link,date) {
+    constructor(name,author,screen,link,date,pack) {
         this.name = name;
         this.author = author;
         this.screen = screen;
         this.link = link;
         this.date = date;
+        this.pack = pack;
     }
 }
 
 var InfinityBlocks = [
-    new Block("Trees"                   ,"Dragere","/blockimages/DragereTrees.png","/blocks/Trees.zip","22.08.2018"),
-    new Block("8thSphere"               ,"Dragere","/blockimages/8thSphere.png","/blocks/8thSphere.zip","29.08.2018"),
-    new Block("FlatToQuarterPipe"       ,"Dragere","/blockimages/FlatToQuarterPipe.png","/blocks/FlatToQuarterPipe.zip","06.09.2018"),
-    new Block("Portals"                 ,"Dragere","/blockimages/Portals.png","/blocks/Portals.zip","07.09.2018"),
-    new Block("FracturedBlocks"         ,"Dragere","/blockimages/FracturedBlocks.png","/blocks/FracturedBlocks.zip","11.09.2018"),
-
-    new Block("GTA San Andreas Little Props"   ,"ElPollo315","/blockimages/GTASALilttleProps.png","/blocks/GTASA_Volume1Blocks_Folder.zip","18.09.2018"),
-    new Block("GTA San Andreas Planes"   ,"ElPollo315","/blockimages/GTAVC_2_Planes.png","/blocks/GTAVC_2_Planes.zip","19.09.2018"),
-
-    new Block("SpeedUFO"   ,"St1ngLeR","/blockimages/SpeedUFO.jpg","/blocks/SpeedUFO.zip","14.03.2020"),
-    new Block("Lighthouse"   ,"St1ngLeR","/blockimages/Lighthouse.jpg","/blocks/Lighthouse.zip","14.03.2020"),
-    new Block("Icebreaker"   ,"St1ngLeR","/blockimages/Icebreaker.jpg","/blocks/Icebreaker.zip","12.05.2020"),
-    new Block("Balloons"   ,"St1ngLeR","/blockimages/Balloons.jpg","/blocks/Balloons.zip","14.05.2020"),
-    new Block("Platform To Highway"   ,"St1ngLeR","/blockimages/PlatformToHighway.jpg","/blocks/PlatformToHighway.zip","17.05.2020"),
-    new Block("Circuit Trap"   ,"St1ngLeR","/blockimages/Circuit_Trap.jpg","/blocks/Circuit_Trap.zip","23.05.2020"),
-    new Block("Dirt Road Pillar"   ,"St1ngLeR","/blockimages/Dirt_Road_Pillar.jpg","/blocks/Dirt_Road_Pillar.zip","23.05.2020")
-
+    new Block(
+        "Trees",
+        "Dragere",
+        "/blockimages/DragereTrees.png",
+        "/blocks/Trees.zip",
+        "22.08.2018",
+        "PACK"
+    ),
+    new Block(
+        "8thSphere",
+        "Dragere",
+        "/blockimages/8thSphere.png",
+        "/blocks/8thSphere.zip",
+        "29.08.2018",
+        "1 BLOCK"
+    ),
+    new Block(
+        "FlatToQuarterPipe",
+        "Dragere",
+        "/blockimages/FlatToQuarterPipe.png",
+        "/blocks/FlatToQuarterPipe.zip",
+        "06.09.2018",
+        "BLOCK"
+    ),
+    new Block(
+        "Portals",
+        "Dragere",
+        "/blockimages/Portals.png",
+        "/blocks/Portals.zip",
+        "07.09.2018",
+        "2 BLOCKS"
+    ),
+    new Block(
+        "FracturedBlocks",
+        "Dragere",
+        "/blockimages/FracturedBlocks.png",
+        "/blocks/FracturedBlocks.zip",
+        "11.09.2018",
+        "3 BLOCKS"
+    ),
+    new Block(
+        "GTA San Andreas Little Props",
+        "ElPollo315",
+        "/blockimages/GTASALilttleProps.png",
+        "/blocks/GTASA_Volume1Blocks_Folder.zip",
+        "18.09.2018",
+        "PACK"
+    ),
+    new Block(
+        "GTA San Andreas Planes",
+        "ElPollo315",
+        "/blockimages/GTAVC_2_Planes.png",
+        "/blocks/GTAVC_2_Planes.zip",
+        "19.09.2018",
+        "PACK"
+    ),
+    new Block(
+        "SpeedUFO",
+        "St1ngLeR",
+        "/blockimages/SpeedUFO.jpg",
+        "/blocks/SpeedUFO.zip",
+        "14.03.2020",
+        "BLOCK"
+    ),
+    new Block(
+        "Lighthouse",
+        "St1ngLeR",
+        "/blockimages/Lighthouse.jpg",
+        "/blocks/Lighthouse.zip",
+        "14.03.2020",
+        "BLOCK"
+    ),
+    new Block(
+        "Icebreaker",
+        "St1ngLeR",
+        "/blockimages/Icebreaker.jpg",
+        "/blocks/Icebreaker.zip",
+        "12.05.2020",
+        "BLOCK",
+    ),
+    new Block(
+        "Balloons",
+        "St1ngLeR",
+        "/blockimages/Balloons.jpg",
+        "/blocks/Balloons.zip",
+        "14.05.2020",
+        "BLOCK"
+    ),
+    new Block(
+        "Platform To Highway",
+        "St1ngLeR",
+        "/blockimages/PlatformToHighway.jpg",
+        "/blocks/PlatformToHighway.zip",
+        "17.05.2020",
+        "BLOCK"
+    ),
+    new Block(
+        "Circuit Trap",
+        "St1ngLeR",
+        "/blockimages/Circuit_Trap.jpg",
+        "/blocks/Circuit_Trap.zip",
+        "23.05.2020",
+        "BLOCK"
+    ),
+    new Block(
+        "Dirt Road Pillar",
+        "St1ngLeR",
+        "/blockimages/Dirt_Road_Pillar.jpg",
+        "/blocks/Dirt_Road_Pillar.zip",
+        "23.05.2020",
+        "BLOCK"
+    ),
+    new Block(
+        "LinuxCat_Blocks",
+        "LinuxCat",
+        "",
+        "/blocks/LinuxCats_Blocks.zip",
+        "2.11.2020",
+        "PACK"
+    ),
+    new Block(
+        "sausage1",
+        "St1ngLeR",
+        "",
+        "/blocks/sausage1.zip",
+        "17.04.2021",
+        "PACK"
+    ),
+    new Block(
+        "GridPlatforms",
+        "St1ngLeR",
+        "/blockimages/GridPlatforms.png",
+        "/blocks/Grid_Platforms.zip",
+        "17.04.2021",
+        "PACK"
+    ),
+    new Block(
+        "Holes",
+        "St1ngLeR",
+        "",
+        "/blocks/Holes.zip",
+        "17.04.2021",
+        "PACK"
+    ),
+    new Block(
+        "Ramps",
+        "St1ngLeR",
+        "/blockimages/Ramps.png",
+        "/blocks/Ramps.zip",
+        "18.04.2021",
+        "PACK"
+    ),
 ];
 
 module.exports = { InfinityBlocks };
