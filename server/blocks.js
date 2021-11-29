b9d5b5a8229a8352ba4e823796d3d08cda470cff
Bartek <ketrab508@gmail.com>
2021-11-29 13:29:43 +0100

    Interface and Database update

--- server/blocks.js
+++ server/blocks.js
@@ -1,39 +1,164 @@
 
 class Block {
-    constructor(name,author,screen,link,date,pack) {
+    constructor(name,author,screen,link,date,cnt,containList) {
         this.name = name;
         this.author = author;
         this.screen = screen;
         this.link = link;
         this.date = date;
-        this.pack = pack;
+        this.cnt = cnt;
+        this.containList = containList;
     }
 }
 
 var InfinityBlocks = [
+
+
+    new Block(
+        "Minecraft",
+        "Tomek555",
+        "/blockimages/Minecraft.png",
+        "/blocks/Minecraft.zip",
+        "24.05.2018",
+        "16",
+        [
+            'Minecraft16 > Cobblestone',
+            'Minecraft16 > Dirt',
+            'Minecraft16 > Grass',
+            'Minecraft16 > Leaves',
+            'Minecraft16 > Plank',
+            'Minecraft16 > Sand',
+            'Minecraft16 > Stone',
+            'Minecraft16 > Wood',
+            'Minecraft32 > Cobblestone',
+            'Minecraft32 > Dirt',
+            'Minecraft32 > Grass',
+            'Minecraft32 > Leaves',
+            'Minecraft32 > Plank',
+            'Minecraft32 > Sand',
+            'Minecraft32 > Stone',
+            'Minecraft32 > Wood',
+        ]
+    ),
+
+    new Block(
+        "ESL Titlepack",
+        "Tomek",
+        "/blockimages/esltitlepack.jpg",
+        "/blocks/ESLTitlepack.zip",
+        "26.07.2018",
+        "93",
+        [
+            'Block0X > Curve 1-5',
+            'Block0X > Slope 1-6',
+            'Block0X > Slope Transitions 1-4',
+            'Block0X > Transition A 1-5',
+            'Block0X > Transition B 1-5',
+            'Block0X > Transition C 1-3',
+            'Block0X > Transition D 1-3',
+            '-- where X in {1,2,3}',
+        ]
+    ),
+    new Block(
+        "TM Logo & Ice Block",
+        "Ketrab",
+        "/blockimages/TMLogoBlocks.png",
+        "/blocks/Ketrab.zip",
+        "24.07.2018",
+        "3",
+        ['TM Logo T','TM Logo M','Stadium Ice Block']
+    ),
+    new Block(
+        "Tubes",
+        "Ketrab",
+        "/blockimages/Tubes.png",
+        "/blocks/Tubes.zip",
+        "24.07.2018",
+        "62",
+        [
+            'Simple > Normal',
+            'Simple > Checkpoint',
+            'Simple > Finish',
+            'Simple > Ice',
+            'Simple > Boost',
+            'Simple > Boost Half',
+            'Simple > Freewheeling',
+            'Simple > Freewheeling Half',
+            'Simple > Mix Boost-Freewheeling',
+            'Curve > 1x1',
+            'Curve > 2x2',
+            'Curve > 3x3',
+            'Curve > 4x4',
+            'Descent > Simple',
+            'Descent > Normal To Descent',
+            'Descent > Descent To Normal',
+            'Descent > SlopeOne',
+            'Descent > BoostUp',
+            'Descent > BoostDown',
+            'Descent > Freewheeling',
+            'Descent > Checkpoint Up',
+            'Descent > Checkpoint Down',
+            'Descent > Normal To Descent Curve 2x2',
+            'Descent > Normal To Descent Curve 2x2 Mirror',
+            'Descent > Descent To Normal Curve 2x2',
+            'Descent > Descent To Normal Curve 2x2 Mirror',
+            'Descent > Normal To Descent Curve 3x3',
+            'Descent > Normal To Descent Curve 3x3 Mirror',
+            'Descent > Descent To Normal Curve 3x3',
+            'Descent > Descent To Normal Curve 3x3 Mirror',
+            'Other > SType 2x3',
+            'Other > SType 2x3 Mirror',
+            'Other > SType 2x4',
+            'Other > SType 2x4 Mirror',
+            'Other > SType 2x5',
+            'Other > SType 2x5 Mirror',
+            'Other > Cross Y',
+            'Other > Cross X',
+            'Other > Cross T',
+            'Switch > To Road',
+            'Switch > To Circuit',
+            'Switch > To Circuit Long',
+            'Switch > To Up Ramp 2x2',
+            'Switch > Normal To Rotated',
+            'Halfpipe > Simple',
+            'Halfpipe > Side',
+            'Halfpipe > Curve 2x2',
+            'Halfpipe > Curve 3x3',
+            'Halfpipe > Side Curve 2x2',
+            'Halfpipe > To Road',
+            'Halfpipe > To Circuit',
+            'Dirt > Switch To Dirt',
+            'Dirt > Switch Through Dirt',
+            'Dirt > Special From Dirt: D',
+            'Dirt > Special From Dirt: O',
+            'Special > Insider D1',
+            'Special > Insider D2',
+            'Special > Insider D3',
+            'Special > Outsider C1',
+            'Special > Outsider C2',
+            'Special > To Pillar',
+            'Special > Side To Pillar',
+        ]
+    ),
+
+
     new Block(
         "Trees",
         "Dragere",
         "/blockimages/DragereTrees.png",
         "/blocks/Trees.zip",
         "22.08.2018",
-        "PACK"
+        "3",
+        ['Callistemon','JapaneseMaple','Pine']
     ),
     new Block(
-        "8thSphere",
+        "Circuit Ramps",
         "Dragere",
-        "/blockimages/8thSphere.png",
-        "/blocks/8thSphere.zip",
-        "29.08.2018",
-        "1 BLOCK"
-    ),
-    new Block(
-        "FlatToQuarterPipe",
-        "Dragere",
-        "/blockimages/FlatToQuarterPipe.png",
-        "/blocks/FlatToQuarterPipe.zip",
+        "/blockimages/CircuitRamps.png",
+        "/blocks/CircuitRamps.zip",
         "06.09.2018",
-        "BLOCK"
+        "3",
+        ['8th Sphere','Flat To QuaterPipe','Flat To QuaterPipe Mirrored']
     ),
     new Block(
         "Portals",
@@ -41,15 +166,17 @@ var InfinityBlocks = [
         "/blockimages/Portals.png",
         "/blocks/Portals.zip",
         "07.09.2018",
-        "2 BLOCKS"
+        "2",
+        ['Blue','Orange']
     ),
     new Block(
-        "FracturedBlocks",
+        "Fractured Blocks",
         "Dragere",
         "/blockimages/FracturedBlocks.png",
         "/blocks/FracturedBlocks.zip",
         "11.09.2018",
-        "3 BLOCKS"
+        "3",
+        ['f0','f1','f2']
     ),
     new Block(
         "GTA San Andreas Little Props",
@@ -57,7 +184,33 @@ var InfinityBlocks = [
         "/blockimages/GTASALilttleProps.png",
         "/blocks/GTASA_Volume1Blocks_Folder.zip",
         "18.09.2018",
-        "PACK"
+        "24",
+        [
+            'Industrial Zone > DockBar',
+            'Industrial Zone > BusStop',
+            'Industrial Zone > Hydrant1',
+            'Industrial Zone > Hydrant2',
+            'Industrial Zone > MailBox',
+            'Industrial Zone > MailBox2',
+            'Industrial Zone > ParkMeter',
+            'Industrial Zone > Bollard',
+            'Industrial Zone > Cone',
+            'Kick Stadium > Kick Bus',
+            'Kick Stadium > LaFCar1',
+            'Kick Stadium > LaFCar2',
+            'Kick Stadium > LaFCar3',
+            'Kick Stadium > LaFCar4',
+            'Kick Stadium > SWPick',
+            'Kick Stadium > Bleach',
+            'Kick Stadium > KickArse',
+            'Kick Stadium > KickRing',
+            'Kick Stadium > KickWall',
+            'Kick Stadium > KickLoop01',
+            'Kick Stadium > KickRamp01',
+            'Kick Stadium > KickRamp02',
+            'Kick Stadium > KickRamp03',
+            'Kick Stadium > KickRamp04'
+        ]
     ),
     new Block(
         "GTA San Andreas Planes",
@@ -65,15 +218,17 @@ var InfinityBlocks = [
         "/blockimages/GTAVC_2_Planes.png",
         "/blocks/GTAVC_2_Planes.zip",
         "19.09.2018",
-        "PACK"
+        "2",
+        ['Jumbo','Luxor']
     ),
     new Block(
-        "SpeedUFO",
+        "Speed UFO",
         "St1ngLeR",
         "/blockimages/SpeedUFO.jpg",
         "/blocks/SpeedUFO.zip",
         "14.03.2020",
-        "BLOCK"
+        "1",
+        ['UFO']
     ),
     new Block(
         "Lighthouse",
@@ -81,7 +236,8 @@ var InfinityBlocks = [
         "/blockimages/Lighthouse.jpg",
         "/blocks/Lighthouse.zip",
         "14.03.2020",
-        "BLOCK"
+        "1",
+        ['Lighthouse']
     ),
     new Block(
         "Icebreaker",
@@ -89,7 +245,8 @@ var InfinityBlocks = [
         "/blockimages/Icebreaker.jpg",
         "/blocks/Icebreaker.zip",
         "12.05.2020",
-        "BLOCK",
+        "1",
+        ['Icebreaker']
     ),
     new Block(
         "Balloons",
@@ -97,7 +254,8 @@ var InfinityBlocks = [
         "/blockimages/Balloons.jpg",
         "/blocks/Balloons.zip",
         "14.05.2020",
-        "BLOCK"
+        "8",
+        ['Skin 1-8']
     ),
     new Block(
         "Platform To Highway",
@@ -105,7 +263,8 @@ var InfinityBlocks = [
         "/blockimages/PlatformToHighway.jpg",
         "/blocks/PlatformToHighway.zip",
         "17.05.2020",
-        "BLOCK"
+        "2",
+        ['Air','Ground']
     ),
     new Block(
         "Circuit Trap",
@@ -113,7 +272,8 @@ var InfinityBlocks = [
         "/blockimages/Circuit_Trap.jpg",
         "/blocks/Circuit_Trap.zip",
         "23.05.2020",
-        "BLOCK"
+        "3",
+        ['Air','Ground','Slope']
     ),
     new Block(
         "Dirt Road Pillar",
@@ -121,7 +281,8 @@ var InfinityBlocks = [
         "/blockimages/Dirt_Road_Pillar.jpg",
         "/blocks/Dirt_Road_Pillar.zip",
         "23.05.2020",
-        "BLOCK"
+        "2",
+        ['Dirt','Grass']
     ),
     new Block(
         "LinuxCat_Blocks",
@@ -129,23 +290,34 @@ var InfinityBlocks = [
         "",
         "/blocks/LinuxCat_Blocks.zip",
         "2.11.2020",
-        "PACK"
+        "0",
+        []
     ),
     new Block(
-        "sausage1",
+        "Sausage Roads",
         "St1ngLeR",
         "/blockimages/Sausage.png",
         "/blocks/sausage1.zip",
         "17.04.2021",
-        "PACK"
+        "3",
+        ['Corner','Normal To Sausage','Straight']
     ),
     new Block(
-        "GridPlatforms",
+        "Grid Platforms",
         "St1ngLeR",
         "/blockimages/GridPlatforms.png",
         "/blocks/Grid_Platforms.zip",
         "17.04.2021",
-        "PACK"
+        "7",
+        [
+            'Pillars > Pillar',
+            'Pillars > Pillar Base',
+            'Platforms > Base',
+            'Platforms > Free Wheeling',
+            'Platforms > Slope',
+            'Platforms > Turbo',
+            'Platforms > Turbo Roulette',
+        ]
     ),
     new Block(
         "Holes",
@@ -153,7 +325,14 @@ var InfinityBlocks = [
         "/blockimages/Holes.png",
         "/blocks/Holes.zip",
         "17.04.2021",
-        "PACK"
+        "5",
+        [
+            'Hole 30 Degrees',
+            'Roulette Tubro Hole Air',
+            'Roulette Tubro Hole Ground',
+            'Turbo Hole Air',
+            'Turbo Hole Ground',
+        ]
     ),
     new Block(
         "Ramps",
@@ -161,23 +340,26 @@ var InfinityBlocks = [
         "/blockimages/Ramps.png",
         "/blocks/Stadium_Fabric_Ramps.zip",
         "18.04.2021",
-        "PACK"
+        "4",
+        ['Fabric Ramp 1-4']
     ),
     new Block(
-        "StadiumPlatformLoopStartCorner",
+        "Stadium Platform Loop-Start-Corner",
         "St1ngLeR",
         "/blockimages/StadiumPlatformLoopStartCorner.png",
         "/blocks/StadiumPlatformLoopStartCorner.zip",
         "28.06.2021",
-        "BLOCK"
+        "1",
+        ['Stadium Platform Loop-Start-Corner']
     ),
     new Block(
-        "Coconut Mall",
+        "Coconut Mall (Scenery)",
         "LinuxCat",
-        "",
+        "/blockimages/CoconutMall.png",
         "/blocks/Coconut_Mall.zip",
         "02.07.2021",
-        "BLOCK"
+        "1",
+        ['Coconut Mall - Mariocart']
     ),
     new Block(
         "Numbers",
@@ -185,15 +367,34 @@ var InfinityBlocks = [
         "/blockimages/Numbers.png",
         "/blocks/Numbers.zip",
         "06.07.2021",
-        "PACK"
+        "80",
+        [
+            'Black > 1-9',
+            'Blue > 1-9',
+            'Forest > 1-9',
+            'Green > 1-9',
+            'Grey > 1-9',
+            'Orange > 1-9',
+            'Red > 1-9',
+            'White > 1-9',
+            'Zeros > All colors',
+        ]
     ),
     new Block(
-        "IceRoad",
+        "Ice Road",
         "LinuxCat",
         "/blockimages/IceRoad.png",
         "/blocks/IceRoad.zip",
         "16.11.2021",
-        "PACK"
+        "6",
+        [
+            'Ice Road Straight',
+            'Ice Road Curve 2',
+            'Ice Road Curve 3',
+            'Ice Road Curve 4',
+            'Ice Road Curve 5',
+            'Red Turbo',
+        ]
     ),
     new Block(
         "Platforms",
@@ -201,7 +402,14 @@ var InfinityBlocks = [
         "/blockimages/Platforms.png",
         "/blocks/Platforms.zip",
         "16.11.2021",
-        "PACK"
+        "5",
+        [
+            'Dirt Block',
+            'Grass block',
+            'Ice block',
+            'Water block',
+            'Red Turbo',
+        ]
     ),
 ];
 
